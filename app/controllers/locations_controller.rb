223bae0b4192503d428e195895dec34dd5c1978c
= <=>
2020-08-14 11:59:14 -0400

    list discs for location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,11 +1,25 @@
 class LocationsController < ApplicationController
   def index
-    @locations = Location.all
+    @locations = Location.all_but_default
   end
 
   def new
     @location = Location.new
   end
+
+  def edit
+    @location = Location.find params[:id]
+  end
+
+  def update
+    @location = Location.find params[:id]
+    if @location.update location_params
+      redirect_to @location
+    else
+      render "edit"
+    end
+  end
+
 
   def create
     @location = Location.new(location_params)
