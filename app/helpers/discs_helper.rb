111a0adf4a8d9c921d8e2dd87d9f29d61c297e90
brachsmithi <=>
2020-08-20 13:43:11 -0400

    sort discs in the index

--- app/helpers/discs_helper.rb
+++ app/helpers/discs_helper.rb
@@ -19,4 +19,22 @@ module DiscsHelper
     end
   end
 
+  def sort_discs disc_arr
+    disc_arr.sort_by { |d| trim_article(display_name(d)).downcase }
+  end
+
+  private
+
+  def trim_article value
+    if value.start_with? 'A '
+      value.delete_prefix 'A '
+    elsif value.start_with? 'An '
+      value.delete_prefix 'An '
+    elsif value.start_with? 'The '
+      value.delete_prefix 'The '
+    else
+      value
+    end
+  end
+
 end
