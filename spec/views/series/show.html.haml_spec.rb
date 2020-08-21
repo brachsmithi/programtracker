8e2aa3df23fc94fa1711d59db7b94334e7a5b033
brachsmithi <=>
2020-08-20 21:53:31 -0400

    sort series programs by sequence number

--- spec/views/series/show.html.haml_spec.rb
+++ spec/views/series/show.html.haml_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe "series/show.html.haml", type: :view do
   before(:each) do
     location = create(:default_location)
     series = create(:series, name: 'Ray Milland Movies')
-    program1 = create(:program, name: 'The Flying Scotsman')
-    program2 = create(:program, name: 'The Lady from the Sea')
+    program1 = create(:program, name: 'The Flying Scotsman', version: 'Full Screen')
+    program2 = create(:program, name: 'The Lady from the Sea', version: 'TV Edit')
     assign(:series, series)
     create(:series_program, series_id: series.id, program_id: program1.id, sequence: 3)
     create(:series_program, series_id: series.id, program_id: program2.id, sequence: 4)
@@ -19,6 +19,8 @@ RSpec.describe "series/show.html.haml", type: :view do
     expect(rendered).to match /Ray Milland Movies/
     expect(rendered).to match /The Flying Scotsman/
     expect(rendered).to match /The Lady from the Sea/
+    expect(rendered).to match /Full Screen/
+    expect(rendered).to match /TV Edit/
   end
 
   it 'displays all boilerplate' do
