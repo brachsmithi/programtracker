e7bfa48acf93feea42309c42f7b9aebf1417a481
brachsmithi <=>
2020-08-21 16:29:50 -0400

    clean the pagination css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import 'css/site'
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -10,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require('jquery')
 require('packs/cocoon.js')
+require('css/site')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
