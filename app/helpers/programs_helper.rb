21d56e7a742afa47d50989b42198e8228bdda935
brachsmithi <=>
2020-08-22 16:32:24 -0400

    add year to program capsule data

--- app/helpers/programs_helper.rb
+++ app/helpers/programs_helper.rb
@@ -1,10 +1,11 @@
 module ProgramsHelper
 
   def program_capsule program
+    year = program.year.blank? ? '' : " (#{program.year})"
     s_names = program.series.pluck(:name)
     series = s_names.any? ? " - #{s_names.join(', ')}" : ''
     version = program.version.blank? ? '' : " (#{program.version})"
-    "#{program.name}#{series}#{version}"
+    "#{program.name}#{year}#{series}#{version}"
   end
 
 end
--- spec/helpers/programs_helper_spec.rb
+++ spec/helpers/programs_helper_spec.rb
@@ -14,36 +14,42 @@ RSpec.describe ProgramsHelper, type: :helper do
 
   context 'program_capsule' do
 
-    it 'shows name, series, and version' do
+    it 'shows name, year, series, and version' do
       bond = create(:series, name: 'James Bond')
-      program = create(:program, name: 'Moonraker', version: 'Full Screen')
+      program = create(:program, name: 'Moonraker', version: 'Full Screen', year: '1979')
       SeriesProgram.create(series_id: bond.id, program_id: program.id)
 
-      expect(helper.program_capsule(program)).to eq('Moonraker - James Bond (Full Screen)')
+      expect(helper.program_capsule(program)).to eq('Moonraker (1979) - James Bond (Full Screen)')
     end
 
     it 'shows multiple series' do
       bond = create(:series, name: 'James Bond')
       moore = create(:series, name: 'Roger Moore Bond')
-      program = create(:program, name: 'Moonraker', version: 'Full Screen')
+      program = create(:program, name: 'Moonraker', version: 'Full Screen', year: '1979')
       SeriesProgram.create(series_id: bond.id, program_id: program.id)
       SeriesProgram.create(series_id: moore.id, program_id: program.id)
 
-      expect(helper.program_capsule(program)).to eq('Moonraker - James Bond, Roger Moore Bond (Full Screen)')
+      expect(helper.program_capsule(program)).to eq('Moonraker (1979) - James Bond, Roger Moore Bond (Full Screen)')
     end
 
-    it 'shows only program and series if there is no version' do
+    it 'shows only program, yaear, and series if there is no version' do
       bond = create(:series, name: 'James Bond')
-      program = create(:program, name: 'Moonraker', version: '')
+      program = create(:program, name: 'Moonraker', version: '', year: '1979')
       SeriesProgram.create(series_id: bond.id, program_id: program.id)
 
-      expect(helper.program_capsule(program)).to eq('Moonraker - James Bond')
+      expect(helper.program_capsule(program)).to eq('Moonraker (1979) - James Bond')
     end
 
-    it 'shows only name and version if there are no series' do
-      program = create(:program, name: 'Moonraker', version: 'Full Screen')\
+    it 'shows only name, year, and version if there are no series' do
+      program = create(:program, name: 'Moonraker', version: 'Full Screen', year: '1979')
 
-      expect(helper.program_capsule(program)).to eq('Moonraker (Full Screen)')
+      expect(helper.program_capsule(program)).to eq('Moonraker (1979) (Full Screen)')
+    end
+
+    it 'shows only name if there is no other data' do
+      program = create(:program, name: 'Moonraker', version: '', year: '')
+
+      expect(helper.program_capsule(program)).to eq('Moonraker')
     end
 
   end
