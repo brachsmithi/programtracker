41196151529930e7ff0b21fb63b22e67642be841
brachsmithi <=>
2020-08-23 08:51:19 -0400

    fix nil comparison in series view

--- app/models/series_program.rb
+++ app/models/series_program.rb
@@ -1,4 +1,9 @@
 class SeriesProgram < ApplicationRecord
   belongs_to :series
   belongs_to :program
+
+  def safe_sequence
+    self.sequence.nil? ?  0 : self.sequence
+  end
+
 end
--- spec/models/series_program_spec.rb
+++ spec/models/series_program_spec.rb
@@ -18,6 +18,20 @@ RSpec.describe SeriesProgram, :type => :model do
     subject.program = nil
     expect(subject).to_not be_valid
   end
+
+  describe 'safe_sequence' do
+    
+    it 'should return sequence if positive' do
+      subject.sequence = 1
+      expect(subject.safe_sequence).to eq 1
+    end
+
+    it 'should return 0 if nil' do
+      subject.sequence = nil
+      expect(subject.safe_sequence).to eq 0
+    end
+
+  end
   
   describe "associations" do
     it { should belong_to(:series).without_validating_presence }
