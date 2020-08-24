150cea3836383b7f82f71b863421d1e7991ea24f
brachsmithi <=>
2020-08-24 09:48:46 -0400

    clean program display for disc edit screens

--- app/helpers/discs_helper.rb
+++ app/helpers/discs_helper.rb
@@ -1,3 +1,9 @@
 module DiscsHelper
 
+  def program_select_name program
+    year = program.year.blank? ? '' : " (#{program.year})"
+    version = program.version.blank? ? '' : " - #{program.version}"
+    "#{program.name}#{year}#{version}"
+  end
+
 end
