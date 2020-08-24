f118ccfb81d47757e4b903337485dafd1b647b39
brachsmithi <=>
2020-08-24 09:25:55 -0400

    add programs to show director

--- spec/views/director/show.html.haml_spec.rb
+++ spec/views/director/show.html.haml_spec.rb
@@ -53,5 +53,35 @@ RSpec.describe "directors/show.html.haml", type: :view do
 
   end
   
+  context 'with director that has programs' do
+
+    before(:each) do
+      director = create(:director, name: 'Edward D. Wood, Jr')
+      program1 = create(:program, name: 'Glen or Glenda?')
+      program2 = create(:program, name: 'Plan 9 from Outer Space')
+      create(:programs_director, director_id: director.id, program_id: program1.id)
+      create(:programs_director, director_id: director.id, program_id: program2.id)
+      assign(:director, director)
+    end
+    
+    it 'displays all director names' do
+
+      render
+
+      expect(rendered).to match /Edward D. Wood, Jr/
+      expect(rendered).to match /Glen or Glenda?/
+      expect(rendered).to match /Plan 9 from Outer Space/
+    end
+
+    it 'displays all boilerplate' do
+
+      render
+
+      expect(rendered).to match /Director/
+      expect(rendered).to match /Directors/
+    end
+
+  end
+  
 end
 
