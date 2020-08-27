f54743098e0de3e9bec3397beaef2c5717599dab
brachsmithi <bsmithi.impaired@gmail.com>
2020-08-27 17:20:55 -0400

    create routes for program reports

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   resources :series
   resources :packages
   resources :alternate_titles
+  
+  #reports
+  get 'reports/program/duplicates', to: 'program#duplicates_report'
+  get 'reports/program/unused', to: 'program#unused_report'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
