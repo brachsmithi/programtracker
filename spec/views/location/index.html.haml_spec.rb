1d02436b2f265aa8520abebadf5aa13c0af27b41
brachsmithi <bsmithi.impaired@gmail.com>
2020-08-29 09:37:36 -0400

    display disc count on location index

--- spec/views/location/index.html.haml_spec.rb
+++ spec/views/location/index.html.haml_spec.rb
@@ -5,9 +5,19 @@ RSpec.describe "locations/index.html.haml", type: :view do
   context 'with locations' do
 
     before(:each) do
+      location1 = Location.create(name: 'A-1')
+      create(:disc, location_id: location1.id)
+      create(:disc, location_id: location1.id)
+      create(:disc, location_id: location1.id)
+      location2 = Location.create(name: 'A-2')
+      create(:disc, location_id: location2.id)
+      create(:disc, location_id: location2.id)
+      create(:disc, location_id: location2.id)
+      create(:disc, location_id: location2.id)
+      create(:disc, location_id: location2.id)
       assign(:locations, [
-        Location.create!(name: 'A-1'),
-        Location.create!(name: 'A-2')
+        location1,
+        location2
       ])
       allow(view).to receive_messages(:will_paginate => nil)
     end
@@ -16,8 +26,8 @@ RSpec.describe "locations/index.html.haml", type: :view do
 
       render
 
-      expect(rendered).to match /A-1/
-      expect(rendered).to match /A-2/
+      expect(rendered).to match /A-1 \(3\)/
+      expect(rendered).to match /A-2 \(5\)/
     end
 
     it 'displays all boilerplate' do
