82373fda2062e3a989543c3fe0ebfe4a7f836dae
brachsmithi <bsmithi.impaired@gmail.com>
2020-08-29 21:52:03 -0400

    add route for no disc package report

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get 'reports/program/duplicates', to: 'programs#duplicates_report'
   get 'reports/program/unused', to: 'programs#unused_report'
   get 'reports/disc/no_location', to: 'discs#no_location_report'
+  get 'reports/package/no_discs', to: 'packages#no_discs_report'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
