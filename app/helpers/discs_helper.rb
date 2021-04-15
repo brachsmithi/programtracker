e09fd978b58f8df716dfab79af65095ae976a559
brachsmithi <bsmithi.impaired@gmail.com>
2021-04-14 20:33:13 -0400

    add package name to disc index display

--- app/helpers/discs_helper.rb
+++ app/helpers/discs_helper.rb
@@ -4,4 +4,9 @@ module DiscsHelper
     DiscsSearch.find(disc.id).display_title
   end
 
+  def disc_index_capsule discs_search
+    package = discs_search.package_name.nil? ? "" : "[#{discs_search.package_name}] "
+    "#{package}#{discs_search.display_title} - #{discs_search.format} (#{discs_search.location.name})"
+  end
+
 end
--- spec/helpers/discs_helper_spec.rb
+++ spec/helpers/discs_helper_spec.rb
@@ -12,4 +12,26 @@ RSpec.describe DiscsHelper, type: :helper do
 
   end
 
+  describe 'disc_index_capsule' do
+    
+    it 'should include title, disc format, and location' do
+      loc = create(:location, name: 'A-2')
+      d = create(:disc, format: 'Blu-ray', location: loc)
+      prog = create(:program, name: 'Anaconda', year: '1997')
+      create(:disc_program, disc: d, program: prog)
+
+      expect(helper.disc_index_capsule(DiscsSearch.find(d.id))).to eq 'Anaconda (1997) - Blu-ray (A-2)'
+    end
+    
+    it 'should include package where available' do
+      loc = create(:location, name: 'Col-14')
+      d = create(:disc, name: '1', format: 'DVD', location: loc)
+      package = create(:package, name: 'Rozen Maiden')
+      create(:disc_package, disc: d, package: package, sequence: 1)
+
+      expect(helper.disc_index_capsule(DiscsSearch.find(d.id))).to eq '[Rozen Maiden] 1 - DVD (Col-14)'
+    end
+
+  end
+
 end
