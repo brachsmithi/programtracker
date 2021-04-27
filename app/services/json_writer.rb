9446473b9115f6bda1ed47decb7ce75f57563f35
brachsmithi <bsmithi.impaired@gmail.com>
2021-04-27 19:25:54 -0400

    fix linter errors for services

--- app/services/json_writer.rb
+++ app/services/json_writer.rb
@@ -2,19 +2,15 @@ require './app/services/application_service.rb'
 require 'json'
 
 class JsonWriter < ApplicationService
-  attr_reader :content, :file_name
 
-  def initialize(input)
-    @content = input[:content]
-    @file_name = input[:file_name]
-  end
-
-  def call()
+  def call(input)
+    content = input[:content]
+    file_name = input[:file_name]
     dir = "#{Rails.root}/export"
     FileUtils.mkdir(dir) unless Dir.exists?(dir)
-    file = File.new("#{dir}/#{@file_name}", 'w');
-    file.syswrite(JSON.generate(@content));
-    file.close();
+    file = File.new("#{dir}/#{file_name}", 'w')
+    file.syswrite(JSON.generate(content))
+    file.close
   end
 
 end
--- lib/tasks/data_export.rake
+++ lib/tasks/data_export.rake
@@ -1,13 +1,10 @@
-import 'app/services/json_writer.rb'
-import 'app/services/program_export.rb'
-
 namespace :export do
 
   namespace :json do
 
     desc 'exports programs to json'
     task programs: :environment do
-      ProgramExport.call()
+      ProgramExport.call
     end
 
   end
